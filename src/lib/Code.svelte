d95090e3ad6a5ae5f4d31f92a7911440878fcc91
Daniel Slatkin <danielslatkin@gmail.com>
2023-03-04 06:26:02 +0000

    add Code component

---
+++ src/lib/Code.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+    import Prism from 'prismjs';
+    import 'prismjs/themes/prism.css';
+    import 'prismjs/plugins/normalize-whitespace/prism-normalize-whitespace.js';
+    import 'prismjs/components/prism-typescript.js';
+    import 'prismjs/components/prism-markup.js';
+    import 'prism-svelte';
+
+    export let language: 'html' | 'css' | 'js' | 'ts' | 'svelte';
+    export let source: string;
+
+    $: formatted = format(source);
+
+    const format = (source: string) => {
+        // https://prismjs.com/plugins/normalize-whitespace
+        const trimmed = Prism.plugins.NormalizeWhitespace.normalize(source, {
+            'remove-trailing': true,
+            'remove-indent': true,
+            'left-trim': true,
+            'right-trim': true,
+        });
+
+        return Prism.highlight(trimmed, Prism.languages[language], language);
+    };
+</script>
+
+<pre>
+    <code>{@html formatted}</code>
+</pre>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,28 @@
+<script lang="ts">
+    import Code from '$lib/Code.svelte';
+
+    const source = `
+        <script lang="ts">
+            let test: 'a' | 'b' | () => boolean = undefined
+            console.log(test)
+        ${'</' + 'script>'}
+        
+        <h1>Test</h1>
+
+        <ul>
+            <li>Item 1</li>
+            <li>Item 2</li>
+            <li>Item 3</li>
+        </ul>
+
+        <style>
+            body {
+                background: lightblue;
+            }
+        </style>
+    `;
+</script>
+
 <h1>Welcome to your library project</h1>
 <p>
     Create your package using @sveltejs/package and preview/showcase your work
@@ -6,3 +31,4 @@
 <p>
     Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
 </p>
+<Code language="svelte" {source} />
